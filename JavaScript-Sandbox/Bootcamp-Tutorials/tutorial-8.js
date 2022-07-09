19ee615dcd646c0b5dcc7d94dc712f2c932220a5
hoodaddeveloper <mohammadkhafas@gmail.com>
2022-07-09 22:56:02 +0200

    codding

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-8.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-8.js
@@ -31,3 +31,41 @@ const arrowFunction5 = day => day === 'Wednesday' ? true : false;
 
 console.log(arrowFunction5('Wednesday')); // "true"
 console.log(arrowFunction5('sunday')); // "false"
+
+///// Profile Lookup /////
+
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+
+}
+
+lookUpProfile("Akira", "likes");
