3f70f7503e7d445ba4097260d77a588e40aa6f0e
hoodaddeveloper <mohammadkhafas@gmail.com>
2022-07-09 23:10:39 +0200

    js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-8.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-8.js
@@ -1,7 +1,7 @@
 ///// Function Expressions /////
 
 const plantNeedsWater = function (day) {
-  if (day === 'Wednesday') {
+  if (day === "Wednesday") {
     return true;
   } else {
     return false;
@@ -9,28 +9,28 @@ const plantNeedsWater = function (day) {
 };
 
 plantNeedsWater();
-console.log(plantNeedsWater('Wednesday')); // "true"
+console.log(plantNeedsWater("Wednesday")); // "true"
 
 ///// ES6 Arrow Functions /////
 
 const newFunction = () => {
-  return 'Hello ES6 World';
-}
+  return "Hello ES6 World";
+};
 
 console.log(newFunction()); // "Hello ES6 World"
 
 // Concise Body Arrow Functions
 
 const arrowFunction1 = () => {}; // Zero Parameter
-const arrowFunction2 = parameter => {}; // One Parameter
+const arrowFunction2 = (parameter) => {}; // One Parameter
 const arrowFunction3 = (parameter1, parameter2, parameter3) => {}; // Two or more Parameters
-const arrowFunction4 = number => number + number; // Single line function does not need curly braces
+const arrowFunction4 = (number) => number + number; // Single line function does not need curly braces
 
 // Example:
-const arrowFunction5 = day => day === 'Wednesday' ? true : false;
+const arrowFunction5 = (day) => (day === "Wednesday" ? true : false);
 
-console.log(arrowFunction5('Wednesday')); // "true"
-console.log(arrowFunction5('sunday')); // "false"
+console.log(arrowFunction5("Wednesday")); // "true"
+console.log(arrowFunction5("sunday")); // "false"
 
 ///// Profile Lookup /////
 
@@ -55,7 +55,7 @@ const contacts = [
     number: "0487345643",
     likes: ["Intriguing Cases", "Violin"],
   },
-  
+
   {
     firstName: "Kristian",
     lastName: "Vos",
@@ -65,7 +65,14 @@ const contacts = [
 ];
 
 function lookUpProfile(name, prop) {
-
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      return contacts[i][prop] || "No such Property";
+    }
+  }
+  return "No such Contact";
 }
 
-lookUpProfile("Akira", "likes");
+let data = lookUpProfile("Kristian", "likes");
+
+console.log(data);
