455de8e776816d1beac76adee52a022a4d2317d4
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-10 18:13:16 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-3.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-3.js
@@ -15,3 +15,21 @@ function myFunction2(name2, age2) {
 
 let message = myFunction2('Ali', 20);
 console.log(message); // "Hello Ali, you are 20 years old."
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+let number1 = 8;
+let number2 = 20;
+
+console.log(number1++);
+console.log(number1);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+function myFunction3(num3, num4) {
+  var num3 = 2;
+  var num4 = 4;
+  console.log(num3 === num4);
+}
+
+myFunction3();
