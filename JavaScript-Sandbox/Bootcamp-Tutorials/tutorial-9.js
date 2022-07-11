a8af1f8249bf78e0a450866a510c14f14ee7b26d
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-12 00:18:01 +0200

    learning...

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-9.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-9.js
@@ -37,3 +37,8 @@ var myConcat = function(arr1, arr2) {
 };
 
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+// Write Arrow Functions with Parameters 3.2:
+
+const myConcat2 = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat2([1, 2], [3, 4, 5]));
