1ed69fd99d83ec2ea20c534de2576c5480aef839
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-12 00:11:00 +0200

    js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-9.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-9.js
@@ -12,4 +12,28 @@ function checkScope() {
   return i;
 }
 
-// Mutate an Array Declared with const
+// Write Arrow Functions with Parameters 1.1:
+
+const doubler = (item) => item * 2; // ES6 Arrow Function
+console.log(doubler(4)); // 8
+
+// Write Arrow Functions with Parameters 1.2:
+
+function doubler2(i1, i2) { // ES5 Function
+return i1 * i2;
+}
+
+console.log(doubler2(3, 4)); // 12
+
+// Write Arrow Functions with Parameters 2:
+
+const multiplier = (item2, multi) => item2 * multi; // ES6 Arrow Function
+console.log(multiplier(5, 2)); // 10
+
+// Write Arrow Functions with Parameters 3.1:
+
+var myConcat = function(arr1, arr2) {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
