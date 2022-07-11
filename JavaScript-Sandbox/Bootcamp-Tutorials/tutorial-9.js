31cb3e2530f857096703c4b2b498e4a35d6ca112
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-12 00:23:29 +0200

    learning js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-9.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-9.js
@@ -3,12 +3,12 @@
 // Compare Scopes of the var and let Keywords
 
 function checkScope() {
-  let i = 'function scope'; // var i
+  let i = "function scope"; // var i
   if (true) {
-  let i = 'block scope'; // var i
-    console.log('Block scope i is: ', i);
+    let i = "block scope"; // var i
+    console.log("Block scope i is: ", i);
   }
-  console.log('Function scope i is: ', i);
+  console.log("Function scope i is: ", i);
   return i;
 }
 
@@ -19,8 +19,9 @@ console.log(doubler(4)); // 8
 
 // Write Arrow Functions with Parameters 1.2:
 
-function doubler2(i1, i2) { // ES5 Function
-return i1 * i2;
+function doubler2(i1, i2) {
+  // ES5 Function
+  return i1 * i2;
 }
 
 console.log(doubler2(3, 4)); // 12
@@ -32,7 +33,7 @@ console.log(multiplier(5, 2)); // 10
 
 // Write Arrow Functions with Parameters 3.1:
 
-var myConcat = function(arr1, arr2) {
+var myConcat = function (arr1, arr2) {
   return arr1.concat(arr2);
 };
 
@@ -42,3 +43,19 @@ console.log(myConcat([1, 2], [3, 4, 5]));
 
 const myConcat2 = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat2([1, 2], [3, 4, 5]));
+
+// Set Default Parameters for Your Functions
+
+// 1:
+
+const greeting = (name = "Anonymous") => "Hello " + name; // ES6 Default Parameter
+
+console.log(greeting("John")); // Hello John
+console.log(greeting()); // "Hello Anonymous"
+
+// 2:
+
+const increment = (number, value) => number + value; 
+
+console.log(increment());
+console.log(increment());
