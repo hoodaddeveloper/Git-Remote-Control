2e25f4d537830ae65cf60a84581a38d4219990d4
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-15 14:36:09 +0200

    js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-9.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-9.js
@@ -109,3 +109,12 @@ console.log(returnSkyColor2()); // blue
 
 // 3:
 
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+function callMyNightSky() {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());
