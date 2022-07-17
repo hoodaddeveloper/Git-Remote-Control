43cfc32ff1fe949de75da7b897cc5f3f210e0f30
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-17 16:16:16 +0200

    js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-10.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-10.js
@@ -32,4 +32,5 @@ let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
 const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
 
 condiments[0] = 'Mayo';
-console.log(condiments[0]);
+console.log(condiments); // ['Mayo', 'Mustard', 'Soy Sauce', 'Sriracha']
+
