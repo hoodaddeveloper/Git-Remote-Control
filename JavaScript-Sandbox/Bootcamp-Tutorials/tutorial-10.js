c83f630833f0679a0cc530cbdc5d3a6ccc4fea9b
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-17 15:17:09 +0200

    Accessing Elements

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-10.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-10.js
@@ -4,3 +4,15 @@ const hobbies = ["Ali", "Hoodad", "Akbar"];
 console.log(hobbies);
 
 ///// Accessing Elements /////
+
+const famousSayings = [
+  'Fortune favors the brave.',
+  'A joke is a very serious thing.',
+  'Where there is love there is life.'
+];
+
+const listItem = famousSayings[0]; // 'Fortune favors the brave.'
+console.log(listItem);
+
+console.log(famousSayings[0]); // 'Fortune favors the brave.'
+console.log(famousSayings[2]); // 'Where there is love there is life.'
