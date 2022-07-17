6e2a9d9b00ce6a79411d459d3d98d468dd0e15af
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-17 16:15:42 +0200

    Arrays with let and const

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-10.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-10.js
@@ -27,3 +27,9 @@ console.log(groceryList); // ['bread', 'avocados', 'milk']
 
 ///// Arrays with let and const /////
 
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments[0]);
