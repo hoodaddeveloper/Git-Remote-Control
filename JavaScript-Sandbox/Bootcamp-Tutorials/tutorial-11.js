63a352974ec80d66bf33f6e67f3f6a8857e93f2d
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-18 14:14:27 +0200

    js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-11.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-11.js
@@ -12,6 +12,6 @@ for (let i = 5; i < 11; i++) {
 
 /////
 
-for (let i = 5; i < 11; i++) {
-  console.log(i); // 5 6 7 8 9 10
+for (let i = 11; i > 0; i--) {
+  console.log(i); // 11 10 9 8 7 6 5 4 3 2 1 0
 }
