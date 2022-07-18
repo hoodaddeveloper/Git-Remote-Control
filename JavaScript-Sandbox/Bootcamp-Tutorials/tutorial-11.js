088cd9824b5d06160d014e095745516bfecc234d
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-18 14:20:59 +0200

    Looping through Arrays

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-11.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-11.js
@@ -4,14 +4,23 @@ for (let counter = 0; counter < 4; counter++) {
   console.log(counter); // 0 1 2 3
 }
 
-/////
+// / //
 
 for (let i = 5; i < 11; i++) {
   console.log(i); // 5 6 7 8 9 10
 }
 
-/////
+// / //
 
 for (let i = 11; i > 0; i--) {
   console.log(i); // 11 10 9 8 7 6 5 4 3 2 1 0
 }
+
+///// Looping through Arrays /////
+
+const animals = ['Grizzly Bear', 'Sloth', 'Sea Lion'];
+for (let i = 0; i < animals.length; i++){
+  console.log(animals[i]); // Grizzly Bear Sloth Sea Lion
+}
+
+// / //
