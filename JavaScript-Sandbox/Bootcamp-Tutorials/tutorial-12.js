29b1641470b08d47e2ba5a1a4255c4d9e1484341
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-19 15:26:31 +0200

    fcc

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-12.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-12.js
@@ -1,2 +1,8 @@
-///// Basic Data Structures /////
+///// FreeCodeCamp Basic Data Structures /////
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = ["Ali"];
+// Only change code above this line
+console.log(myArray);
 
