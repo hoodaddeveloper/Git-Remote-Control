2aa48c5684b3be9921336859088206b7b6789a6a
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-19 15:31:03 +0200

    Remove Items from an Array with pop() and shift()

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-12.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-12.js
@@ -20,3 +20,10 @@ console.log(mixedNumbers(['IV', 5, 'six']));
 
 /// // ///
 
+function popShift(arr) {
+  let popped = arr.pop(); // Change this line
+  let shifted = arr.shift(); // Change this line
+  return [shifted, popped];
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
