1bc2a787c3043fef6db1cee8907961248a92518e
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-19 15:29:42 +0200

    fcc

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-12.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-12.js
@@ -6,3 +6,17 @@ myArray[1] = ["Ali"];
 // Only change code above this line
 console.log(myArray);
 
+/// // ///
+
+function mixedNumbers(arr) {
+  // Only change code below this line
+arr.push(7, 'VIII', 9);
+arr.unshift('I', 2, 'three');
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+/// // ///
+
