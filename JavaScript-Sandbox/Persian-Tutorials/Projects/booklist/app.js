033c540c0fd3d932bf9666bd22b60bf818250073
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-21 17:25:23 +0200

    training

--- JavaScript-Sandbox/Persian-Tutorials/Projects/booklist/app.js
+++ JavaScript-Sandbox/Persian-Tutorials/Projects/booklist/app.js
@@ -1,8 +1,8 @@
-let pageBanner = document.getElementById("page-banner");
-let titles = document.getElementsByClassName("title");
-let lis = document.getElementsByTagName("li");
-let qSelector = document.querySelector("#page-banner");
+let pageBanner = document.getElementById('page-banner'); // get element by id
+let titles = document.getElementsByClassName('title'); // get element by class
+let lis = document.getElementsByTagName('li');  // get element by tag
 
-qSelector = document.querySelectorAll(".title");
-qSelector = document.querySelectorAll("li");
-qSelector = document.querySelectorAll("#book-list li .name") [1];
+for (let element of lis) { // get elements of list by tag
+  console.log(element);
+}
+
