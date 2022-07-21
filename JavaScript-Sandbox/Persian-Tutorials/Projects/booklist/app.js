a4e390b51c0946101b6f356a14a1d7f08359a53a
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-21 16:27:01 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/Projects/booklist/app.js
+++ JavaScript-Sandbox/Persian-Tutorials/Projects/booklist/app.js
@@ -1,4 +1,3 @@
 let pageBanner = document.getElementById("page-banner");
 let titles = document.getElementsByClassName("title");
-
-console.log(titles);
+let lis = document.getElementsByTagName("li");
