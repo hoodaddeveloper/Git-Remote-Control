2532ae2062ce841175e862420d524863eb0c8074
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-22 12:28:39 +0200

    console

--- JavaScript-Sandbox/Persian-Tutorials/Projects/booklist/app.js
+++ JavaScript-Sandbox/Persian-Tutorials/Projects/booklist/app.js
@@ -1,12 +1,12 @@
-// First way to get elements:
+/* // First way to get elements:
 
-/* let pageBanner = document.getElementById('page-banner'); // get element by id
+let pageBanner = document.getElementById('page-banner'); // get element by id
 let titles = document.getElementsByClassName('title'); // get element by class
 let lis = document.getElementsByTagName('li');  // get element by tag
 
 for (let element of lis) { // get elements of list by tag
   console.log(element);
-} */
+}
 
 // Second way to get elements:
 
@@ -22,12 +22,19 @@ console.log(qSelector3);
 let qSelector4 = document.querySelectorAll("li"); // get all of the elements by tag
 console.log(qSelector4);
 
-/* for (let element of lis) { // get elements of list by tag
+for (let element of lis) { // get elements of list by tag
   console.log(qSelector1);
-} */
+}
 
 let qSelector5 = document.querySelectorAll("#book-list li .name")[2];
 console.log(qSelector5);
 
 let qSelector6 = document.querySelectorAll("#search-books");
-console.log(qSelector6);
+console.log(qSelector6); */
+
+let result;
+const list = document.querySelector("ul");
+const li = document.querySelector("li");
+
+result = li;
+console.log(result);
