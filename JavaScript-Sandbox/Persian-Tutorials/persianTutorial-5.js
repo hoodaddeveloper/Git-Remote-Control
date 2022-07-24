9759736a3a335cd51d5bad00952e75af1cbc9556
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-24 13:31:04 +0200

    Set timeout

---
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-5.js
@@ -0,0 +1,13 @@
+///// Set timeout function to run after 3 seconds /////
+
+console.log(1);
+console.log(2);
+
+setTimeout(function() {
+  console.log("Some Data");	// This will be printed after 3 seconds	
+}, 3000); // 3 seconds
+
+console.log(3);
+console.log(4);
+
+
