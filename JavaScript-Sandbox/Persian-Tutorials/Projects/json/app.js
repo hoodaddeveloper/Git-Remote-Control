e5512ec4ce5b4eba378647fb50dbc5bed16c5619
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-24 14:17:35 +0200

    url

--- JavaScript-Sandbox/Persian-Tutorials/Projects/json/app.js
+++ JavaScript-Sandbox/Persian-Tutorials/Projects/json/app.js
@@ -1,2 +1,19 @@
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+const getTodos = function (url) {
+  const request = new XMLHttpRequest();
 
+  request.open("GET", url);
+  request.send();
+
+  request.onload = function () {
+    if (request.status === 200) {
+      const data = JSON.parse(request.responseText);
+      console.log(data);
+    } else {
+      console.log("request failed");
+    }
+  };
+};
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+getTodos();
