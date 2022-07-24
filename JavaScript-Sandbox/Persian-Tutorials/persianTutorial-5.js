2742438f3dbfeb56dbb671a6e64e1d40d7152a9b
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-24 19:40:42 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-5.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-5.js
@@ -14,9 +14,11 @@ console.log(4);
 
 let timeNow = 20; // Set timeNow to 20
 
-if (timeNow >= 5 && timeNow <= 12) { // If timeNow is greater than or equal to 5 and less than or equal to 12
+if (timeNow >= 5 && timeNow <= 12) {
+  // If timeNow is greater than or equal to 5 and less than or equal to 12
   console.log("Good Morning"); // Print "Good Morning"
-} else if (timeNow >= 13 && timeNow <= 18) { // If timeNow is greater than or equal to 13 and less than or equal to 18
+} else if (timeNow >= 13 && timeNow <= 18) {
+  // If timeNow is greater than or equal to 13 and less than or equal to 18
   console.log("Good Afternoon"); // Print "Good Afternoon"
 } else {
   console.log("Good Night"); // Print "Good Night"
@@ -24,34 +26,3 @@ if (timeNow >= 5 && timeNow <= 12) { // If timeNow is greater than or equal to 5
 
 /////////////////////////////////////////////////////////////////////////
 
-let time = 20; // Set time to 20
-
-
-switch (time) {
-  case (time >= 5 && time <= 12):
-    console.log("Good Morning");
-    break;
-  case (time >= 13 && time <= 18):
-    console.log("Good Afternoon");
-    break;
-  default:
-    console.log("Good Night");
-    break;
-}
-
-
-
-switch (key) {
-  case "a":
-    console.log("A");
-    break;
-  case "b":
-    console.log("B");
-    break;
-  case "c":
-    console.log("C");
-    break;
-  default:
-    console.log("Default");
-    break;
-}
