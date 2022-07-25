d4161caad55778c523d3251de127c8fda04781a5
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-25 19:36:26 +0200

    Constructor Functions

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-6.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-6.js
@@ -32,9 +32,60 @@ let myFriend = {
   age: 30, // Number
   isMarried: false, // Boolean
   hobbies: ["Sports", "Cooking"], // Array
-  address: { // Object
+  address: {
+    // Object
     city: "Shiraz",
     street: "Koshan",
     houseNumber: "14",
   },
+  game: function () {
+    // Function
+    console.log("I love Games");
+  },
 };
+
+myFriend.game();
+
+///// Factory Fuctions /////////////////////////////////////////////////////////
+
+function createHoodad() {
+  return {
+    name: "Hoodad",
+    age: 27,
+    isMarried: true,
+    hobbies: "Game",
+    address: "Shiraz",
+  };
+}
+
+let object = createHoodad();
+console.log(object);
+
+///// Constructor Functions /////////////////////////////////////////////////////
+
+function Circle(radius) {
+  this.radius = radius;
+  this.object = function () {
+    console.log("I am a circle");
+  };
+}
+
+const circle = new Circle(3);
+
+console.log(circle);
+
+/////////////////////////////////////////////////////////////////////////////////
+console.log("//////////////////////////////");
+/////////////////////////////////////////////////////////////////////////////////
+
+const circle2 = {
+  radius2: 1,
+};
+
+delete circle2.radius2;
+delete circle2.draw2;
+
+circle2.color = "red";
+circle.draw2 = function () {};
+
+console.log(circle2);
