439bd3e260b05d832ebd8164ffdf5f9c135abfd6
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-25 15:35:00 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-6.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-6.js
@@ -1,2 +1,24 @@
 ///// Break & Continue ///////////////////////////////////////////////
 
+let i = 0;
+
+while (i <= 10) { // i = 0, i = 1, i = 2, ..., i = 10
+  if (i == 5) { // i = 5
+    break; // break from the loop
+  }
+  console.log(i);
+  i++;
+}
+
+/// /// ///
+
+while (i <= 20) {
+  if (i == 10) {
+    i++;
+    continue; // continue to the next iteration of the loop
+  }
+  console.log(i);
+  i++;
+}
+
+/// /// /// 
