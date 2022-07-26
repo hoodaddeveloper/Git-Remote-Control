1df66f8b1832a3b21c8478db3d667b18c64c41f4
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-26 16:29:18 +0200

    The .map() Method

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-13.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-13.js
@@ -6,3 +6,17 @@ fruits.forEach(item => console.log("I want to eat a " + item));
 fruits.forEach(function(item) {
   console.log(`I want to eat a ${item}`);
 });
+
+///// The .map() Method ////////////////////////////////////////////////////////////////////
+
+const numbers = [1, 2, 3, 4, 5]; 
+ 
+const bigNumbers = numbers.map(number => {
+  return number * 100 + 10 - 2;
+});
+
+console.log(numbers);
+console.log(bigNumbers);
+
+/// /// ///
+
