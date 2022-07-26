12449ce7e807a34a0655bbb7701ab5b69d4a2e01
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-26 23:55:42 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-6.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-6.js
@@ -126,23 +126,27 @@ console.log(y); */
 
 let number1 = 10;
 
-const increase1 = increaseFunc1 = (number1) => number1++;
+const increase1 = (increaseFunc1 = (number1) => number1++);
 console.log(number1); // 10
 
 /// /// /// /// /// ///
 
 let number2 = 10;
 
-const increase2 = increaseFunc2 = (number2) => {
-  number2++
+const increase2 = (increaseFunc2 = (number2) => {
+  number2++;
   console.log(number2);
-};
+});
 
 increaseFunc2(number2); // 11
 
 /// /// /// /// /// ///
 
-let obj1 = {value: 12};
+let obj = { value: 12 };
 
-const increase3 = increaseFunc3 = (obj1) => obj1++;
-console.log(increase3(obj1)); // {value: 12}
+function func(obj) {
+  obj.value++;
+  console.log(obj.value);
+}
+
+func(obj); // 13
