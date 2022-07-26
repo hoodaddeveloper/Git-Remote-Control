048ae9cca55f59965ec7bfe5d4e66a1e832a01b0
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-26 13:53:40 +0200

    Functions as Parameters

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-12.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-12.js
@@ -72,3 +72,19 @@ console.log(isTwoPlusTwo.name);
 
 //// Functions as Parameters /////////////////////////////////////////////////////////
 
+const addTwo = num => {
+  return num + 2;
+}
+
+const checkConsistentOutput = (func, val) => {
+  let checkA = val + 2;
+  let checkB = func(val);
+
+  if (checkA === checkB) {
+    return func(val);
+  } else {
+    console.log('inconsistent results');
+  }
+}
+
+console.log(checkConsistentOutput(addTwo, 12));
