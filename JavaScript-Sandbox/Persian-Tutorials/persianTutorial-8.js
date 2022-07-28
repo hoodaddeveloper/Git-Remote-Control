89adb1d618fb4265d47e77c8fef7f1a4a450b70e
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-28 21:03:12 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-8.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-8.js
@@ -53,3 +53,7 @@ console.log(items);
 
 const html = items.join(" "); // join the items into a string with space
 console.log(html);
+
+const myFriends = ["Sue", "Amanda", "Victoria", "Bob", "Tom"];
+const myFriendsOjbect = myFriends.map((friends) => (obj = { value: friends })); // create an object for each friend
+console.log(myFriendsOjbect);
