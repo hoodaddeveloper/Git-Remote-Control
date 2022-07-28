fbb65e67a550f61315810ef4582b506aaf451ca8
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-28 17:58:23 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-8.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-8.js
@@ -1,9 +1,13 @@
 ///// filter() method /////
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const greaterThanFive = numbers.filter(value => value > 5);
+console.log(greaterThanFive);
 
-numbers.filter(function (value) {
-  return value > 5;
-});
+/// /// /// /// /// ///
 
-console.log(numbers);
+const names = ["Ali", "Hassan", "Hamed", "Reza", "Sara", "Akbar", "Hoodad", "Sue", "Clair", "Leon"];
+const longerThanFour = names.filter(value => value.length > 4);
+console.log(longerThanFour);
+
+/// /// /// /// /// ///
