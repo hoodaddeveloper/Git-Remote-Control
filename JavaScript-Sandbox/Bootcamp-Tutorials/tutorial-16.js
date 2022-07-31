f4468d74a4c65c69e2cfc32f097ee10dee58391b
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-31 14:56:19 +0200

    Bracket Notation

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-16.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-16.js
@@ -4,8 +4,25 @@ let spaceship = {
   homePlanet: 'Earth',
   color: 'silver'
 };
-spaceship.homePlanet; // Returns 'Earth',
-spaceship.color; // Returns 'silver',
+
+console.log(spaceship.homePlanet); // 'Earth'
+console.log(spaceship.color); // silver
 
 /// /// /// /// ///
+
+let spaceShip = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+let crewCount = spaceShip.numCrew;
+let planetArray = spaceShip.flightPath;
+
+console.log(crewCount); // 5
+console.log(planetArray); // ['Venus', 'Mars', 'Saturn']
+
+///// Bracket Notation /////
 
