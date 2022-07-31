614d6f518819b13faac2133987cc60476637f06e
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-31 16:49:32 +0200

    js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-16.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-16.js
@@ -142,3 +142,37 @@ let Ship = {
 
 Ship.retreat();
 Ship.takeOff();
+
+///// Nested Objects /////
+
+const space = {
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032,
+  },
+  crew: {
+    captain: {
+      name: "Sandra",
+      degree: "Computer Engineering",
+      encourageTeam() {
+        console.log("We got this!");
+      },
+    },
+  },
+  engine: {
+    model: "Nimbus2000",
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD",
+    },
+    "back-up": {
+      battery: "Lithium",
+      terabytes: 50,
+    },
+  },
+};
+
+/// /// /// /// /// ///
