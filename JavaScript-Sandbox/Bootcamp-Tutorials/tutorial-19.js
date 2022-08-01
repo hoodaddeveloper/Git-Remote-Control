000980572043c67c2545b994cda10e8c7f8e07fc
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-01 16:03:00 +0200

    js

---
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-19.js
@@ -0,0 +1,44 @@
+///// Learn Intermediate JavaScript /////
+///// Introduction to Classes /////
+
+let halley = {
+  _name: "Halley",
+  _behavior: "Kind",
+
+  get name() {
+    return this._name;
+  },
+
+  get behavior() {
+    return this._behavior;
+  },
+
+  incrementBehavior() {
+    this._behavior++;
+  },
+};
+
+/// / /// / ///
+
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = "Hungry";
+  }
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+const Huppy = new Dog("Huppy");
+console.log(Huppy.name); // Print name value to console
+console.log(Huppy.behavior); // Print behavior value to console
+
+///// Constructor /////
+
