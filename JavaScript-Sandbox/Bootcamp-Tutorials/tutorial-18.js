c1b85933b8dd9e9429f43ef9a089af70561b1001
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-01 15:18:19 +0200

    js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-18.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-18.js
@@ -24,12 +24,58 @@ const robotFactory = (model, mobile) => {
     model: model,
     mobile: mobile,
     beep() {
-      console.log('Beep Boop');
-    }
-  }
-}
+      console.log("Beep Boop");
+    },
+  };
+};
 
-const tinCan = robotFactory('P-500', true);
+const tinCan = robotFactory("P-500", true);
 tinCan.beep();
 
 ///// Property Value Shorthand /////
+
+const monsterFactory2 = (name, age) => {
+  return {
+    name: name,
+    age: age,
+  };
+};
+
+console.log(monsterFactory2("Hoodad", 23));
+
+/// / ///
+const monsterFactory3 = (name, age) => {
+  return {
+    name,
+    age,
+  };
+};
+
+const newAnswer = monsterFactory3("Ali", 27);
+
+/// / ///
+
+function robotFactory4(model, mobile) {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop");
+    },
+  };
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory4("P-501", false);
+console.log(newRobot.model);
+console.log(newRobot.mobile);
+
+/// Use the property value shorthand and refactor the factory function:
+
+const robotFactory5 = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+  };
+};
+
