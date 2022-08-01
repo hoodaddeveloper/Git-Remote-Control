4a89b28a71c340a9aa8c6e7dc9b8bf8b00594793
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-01 13:11:34 +0200

    js

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial.17.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial.17.js
@@ -214,3 +214,17 @@ const project = {
 project.getMission();
 
 /// /// /// /// /// ///
+
+const robot4 = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === "number") {
+      return "My current energy level is " + this._energyLevel;
+    } else {
+      return "System malfunction: cannot retrieve energy level";
+    }
+  },
+};
+
+console.log(robot4.energyLevel);
