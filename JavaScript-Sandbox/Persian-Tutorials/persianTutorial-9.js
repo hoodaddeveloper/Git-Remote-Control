dc0e64dba5122d9657c43a3e1e43f5f52935c0bd
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-02 14:12:19 +0200

    Programming types

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-9.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-9.js
@@ -6,3 +6,28 @@ function start() {
 }
 
 start();
+
+///// Programming types /////
+
+let money = 7000;
+let energie = 99;
+let chance = 90;
+
+function buyMercedes1() {
+  return "You have " + (money * 2 + energie + chance) + " Euro!";
+}
+
+console.log(buyMercedes1()); // 
+
+/// / /// / ///
+
+let buyingCar = {
+  money: 7000,
+  energie: 99,
+  chance: 90,
+  buyMercedes2() {
+    return "You have " + (money * 2 + energie + chance) + " Euro!";
+  }
+}
+
+console.log(buyingCar.buyMercedes2());
