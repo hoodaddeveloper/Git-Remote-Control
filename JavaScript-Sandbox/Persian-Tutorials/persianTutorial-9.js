ea2bfc845645fd5442ded392ca2d469c8ae6a599
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-03 17:52:40 +0200

    Fuctions

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-9.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-9.js
@@ -33,6 +33,19 @@ console.log(friends.length); // 3
 friends[0] = "Kalagar"; // Kalagar
 console.log(friends); // ["Kalagar", "Ali", "Sara"]
 
-friends[3] = "Hoodad"; // Hoodad
-console.log(friends); // ["Kalagar", "Ali", "Sara", "Hoodad"]
+friends[3] = 313; // 313
+console.log(friends); // ["Kalagar", "Ali", "Sara", 313]
 console.log(friends.length); // 4
+
+// Fuctions
+
+function car1(name, model) {
+  console.log(`This is my ${name} ${model}.`);
+}
+
+// Arrow Fuction
+
+car1("BMW", "X7");
+
+let car2 = (name, model) => console.log(`This is my other ${name} ${model}.`);
+car2("Mercedes", "E-Class");
