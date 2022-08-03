eb2b9aa1e6da5289990a161202dc5a24b8dbe42e
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-03 17:34:02 +0200

    Objects

---
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-9.js
@@ -0,0 +1,20 @@
+///// Kalagar Tutorials /////
+
+// Objects
+
+const person = {
+  name: "Hossein",
+  surname: "Kalagar",
+  age: 20,
+  hobbies: ["Sports", "Cooking"],
+};
+
+let getName = person.name;
+let getSurename = person["surname"];
+let getAge = person.age;
+let getHobbies = person["hobbies"];
+
+console.log(getName);
+console.log(getSurename);
+console.log(getAge);
+console.log(getHobbies);
