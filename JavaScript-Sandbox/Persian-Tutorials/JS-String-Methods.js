2eafbcc6278e1bf63b88e8b642557009f3259167
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-09 14:23:31 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/JS-String-Methods.js
+++ JavaScript-Sandbox/Persian-Tutorials/JS-String-Methods.js
@@ -7,3 +7,8 @@ myArray.charCodeAt(); // Returns the Unicode of the character at the specified i
 myArray.concat(); // Joins two or more arrays
 myArray.indexOf(); // Searches the array for an item and returns the index of the item
 myArray.lastIndexOf(); // Searches the array for an item and returns the last index of the item
+myArray.join(); // Joins all elements of an array into a string
+myArray.lastIndexOf(); // Searches the array for an item and returns the last index of the item
+myArray.pop(); // Removes the last element from an array and returns that element
+myArray.push(); // Adds new items to the end of an array and returns the new length
+myArray.reverse(); // Reverses the order of the elements in an array
