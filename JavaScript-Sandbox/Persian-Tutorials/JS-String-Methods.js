8522bd95468633377a3351f92e9e0bff693a64c8
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-09 14:23:20 +0200

    Java Script String Methods List

---
+++ JavaScript-Sandbox/Persian-Tutorials/JS-String-Methods.js
@@ -0,0 +1,9 @@
+///// Java Script String Methods List /////
+
+let myArray = [1, 2, 3, "a", "b", "c"];
+
+myArray.charAt(); // Returns the character at the specified index (position)
+myArray.charCodeAt(); // Returns the Unicode of the character at the specified index (position)
+myArray.concat(); // Joins two or more arrays
+myArray.indexOf(); // Searches the array for an item and returns the index of the item
+myArray.lastIndexOf(); // Searches the array for an item and returns the last index of the item
