652c1865a3b868b31d0fbc39228c87d00c49e4db
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-10 14:43:51 +0200

    js

---
+++ JavaScript-Sandbox/Persian-Tutorials/PT.10.js
@@ -0,0 +1,21 @@
+///// More of loops /////
+
+let p = 0;
+
+while (p < 20) {
+  p++;
+  if (p % 2 === 0) {
+    console.log(p);
+  }
+}
+
+/// /// /// /// ///
+
+let h = 0;
+
+do {
+  h++;
+  if (h % 2 === 0) {
+    console.log(h);
+  }
+} while (h < 20);
