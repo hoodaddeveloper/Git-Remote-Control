58c196753c26266c25aa9d45fbea220762322b25
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-10 14:51:27 +0200

    break;

--- JavaScript-Sandbox/Persian-Tutorials/PT.10.js
+++ JavaScript-Sandbox/Persian-Tutorials/PT.10.js
@@ -20,7 +20,28 @@ let h = 0;
 
 do {
   h++;
+
+  if (h == 10) {
+    break; // break skips the rest of the code in the loop and goes to the next iteration
+  }
+
   if (h % 2 === 0) {
     console.log(h);
   }
 } while (h < 20);
+
+/// /// /// /// ///
+
+let f = 0;
+
+for (let f = 0; f < 30; f++) {
+  if (f == 10) {
+    continue; // continue skips the rest of the code in the loop and goes to the next iteration
+  }
+
+  if (f == 20) {
+    break; // break skips the rest of the code in the loop and goes to the next iteration
+  }
+
+  console.log(f);
+}
