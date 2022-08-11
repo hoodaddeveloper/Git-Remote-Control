d03b2c1eb8096555e2fa830a377ccd0a59136a32
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-11 13:32:48 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/DOM.js
+++ JavaScript-Sandbox/Persian-Tutorials/DOM.js
@@ -3,3 +3,5 @@
 // The DOM is the structure of the web page.
 // The DOM is the tree of elements that make up the web page.
 // Document Object Model (DOM) is a programming interface for HTML and XML documents.
+
+let header = document.addEventListener("header")
