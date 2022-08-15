607ff77bb16c6aaa40231186ccd16646afe7901e
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-15 19:00:08 +0200

    js

--- JavaScript-Sandbox/Persian-Tutorials/DOM.js
+++ JavaScript-Sandbox/Persian-Tutorials/DOM.js
@@ -173,9 +173,14 @@ window.addEventListener("offline", (event) => {
 
 let accordion = document.querySelector(".accordion"); // Get the accordion element.
 Array.from(accordion.children).forEach((wrapper) => {
-  wrapper.querySelector("span").addEventListener("click", e => {
+  wrapper.querySelector("span").addEventListener("click", (e) => {
     let span = e.target;
     let wrapper = span.parentElement;
     wrapper.classList.toggle("show");
-  })
+    Array.from(accordion.children).forEach((w) => {
+      if (w !== wrapper) {
+        w.classList.remove("show");
+      }
+    });
+  });
 });
