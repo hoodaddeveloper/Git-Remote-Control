facb06c34d73e771becd0b7c65257f6c3dbfbf27
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-16 12:54:09 +0200

    .

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-20.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-20.js
@@ -1,2 +1,26 @@
 ///// Classes /////
 
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
