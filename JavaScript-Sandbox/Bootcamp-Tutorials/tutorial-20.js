4ca3114bc104b8f50ada1d6e4d9bc31ea7771832
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-16 12:55:06 +0200

    .

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-20.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-20.js
@@ -26,3 +26,7 @@ class Surgeon {
 
 const surgeonRomero = new Surgeon("Francisco Romero", "Cardiovascular");
 const surgeonJackson = new Surgeon("Ruth Jackson", "Orthopedics");
+
+console.log(surgeonRomero.name);
+surgeonRomero.takeVacationDays(3);
+console.log(surgeonRomero.remainingVacationDays);
