cd105fa33cfb64bc5d64f76eeef9341a54dcacde
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-16 13:21:53 +0200

    .

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-20.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-20.js
@@ -30,3 +30,52 @@ const surgeonJackson = new Surgeon("Ruth Jackson", "Orthopedics");
 console.log(surgeonRomero.name);
 surgeonRomero.takeVacationDays(3);
 console.log(surgeonRomero.remainingVacationDays);
+
+///// Inheritance /////
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+}
+
+const nurseOlynyk = new Nurse("Olynyk", ["Trauma", "Pediatrics"]);
