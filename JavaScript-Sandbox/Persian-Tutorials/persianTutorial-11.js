055207e77d776e101fde0bdf9b91755f4acad35c
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-22 15:34:03 +0200

    constructor

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-11.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-11.js
@@ -1,8 +1,19 @@
 ///// Classes /////
 
-let mypolygon = class polygon {
+let mypolygon1 = class polygon1 {
   constructor(width, height) {
     this.width = width;
     this.height = height;
   }
 };
+
+// Or:
+
+class polygon2 {
+  constructor(width, height) {
+    this.width = width;
+    this.hefight = height;
+  }
+}
+
+mypolygon2 = new polygon2(20, 10)
