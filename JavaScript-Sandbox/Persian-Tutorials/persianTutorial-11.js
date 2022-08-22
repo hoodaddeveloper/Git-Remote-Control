4d5d73fe4de82ca2a60350176e36b1d83bc0a91f
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-22 16:50:07 +0200

    rect

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-11.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-11.js
@@ -9,8 +9,10 @@ let myPolygon1 = class polygon1 {
 
 // Or:
 
-class polygon2 { // this is a block scope
-  constructor(width, height) { // this is a constructor
+class polygon2 {
+  // this is a block scope
+  constructor(width, height) {
+    // this is a constructor
     this.width = width;
     this.height = height;
   }
@@ -24,3 +26,20 @@ let myPolygon2 = new polygon2(20, 40); // this is an instance of the class
 myPolygon2.answer();
 
 console.log(myPolygon2 instanceof polygon2); // this is a instanceof operator
+
+/// /// /// /// ///
+
+class shape {
+  constructor(num) {
+    this.area = num;
+  }
+}
+
+class rectangle extends shape {
+  ShowDetails() {
+    console.log(`The area of the rectangle is: ${this.area}.`);
+  }
+}
+
+let rect = new rectangle(35);
+rect.ShowDetails();
