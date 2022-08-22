8263f63cac28e521804d2a60736b6766d14ea2fb
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-08-22 16:16:46 +0200

    this is a instanceof operator

--- JavaScript-Sandbox/Persian-Tutorials/persianTutorial-11.js
+++ JavaScript-Sandbox/Persian-Tutorials/persianTutorial-11.js
@@ -15,12 +15,12 @@ class polygon2 { // this is a block scope
     this.height = height;
   }
   answer() {
-    console.log(`Polygon's width is: ${this.width}.`);
-    console.log(`Polygon's height is: ${this.height}.`);
+    console.log(`Polygon's width is: ${this.width}.`); // this is a method
+    console.log(`Polygon's height is: ${this.height}.`); // this is a method
   }
 }
 
-let myPolygon2 = new polygon2(20, 40); 
+let myPolygon2 = new polygon2(20, 40); // this is an instance of the class
 myPolygon2.answer();
 
-console.log(myPolygon2 instanceof polygon2); 
+console.log(myPolygon2 instanceof polygon2); // this is a instanceof operator
