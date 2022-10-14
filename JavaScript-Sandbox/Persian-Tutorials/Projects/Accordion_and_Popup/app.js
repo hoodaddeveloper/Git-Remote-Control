c20c67f0cab29f7e5ec4796b25d0e335bc423c94
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-10-14 17:11:18 +0200

    save

--- JavaScript-Sandbox/Persian-Tutorials/Projects/Accordion_and_Popup/app.js
+++ JavaScript-Sandbox/Persian-Tutorials/Projects/Accordion_and_Popup/app.js
@@ -276,11 +276,13 @@ popupCloses.forEach((popupCloses) => {
 // Codes for changing themes
 let theme = document.querySelector(".theme");
 let selection = document.querySelector("#selection");
+let span = document.querySelectorAll("#accordion-element");
 
 Array.from(theme.children).forEach((theme) => {
   theme.addEventListener("click", (e) => {
     let color = e.target.dataset.color;
     document.body.className = color;
     selection.textContent = color;
+    span.style.color = "black";
   });
 });
