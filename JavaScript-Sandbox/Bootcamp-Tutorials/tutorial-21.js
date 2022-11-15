c51dab3fac4c8af2a7426ff2be632b4d771d4218
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-15 14:48:24 +0100

    JAVASCRIPT PROMISES

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-21.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-21.js
@@ -1,2 +1,23 @@
 ///// JAVASCRIPT PROMISES /////
 
+const inventory = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1344,
+};
+
+const myExecutor = (resolve, reject) => {
+  if (inventory.sunglasses > 0) {
+    resolve("Sunglasses order processed.");
+  } else {
+    reject("That item is sold out.");
+  }
+};
+
+const orderSunglasses = () => {
+  return new Promise(myExecutor);
+};
+
+const orderPromise = orderSunglasses();
+
+console.log(orderPromise);
