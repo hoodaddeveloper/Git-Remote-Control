bc0fbe37b58697c06b70a21634c26354c156b01a
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-15 14:58:18 +0100

    .

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-21.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-21.js
@@ -75,3 +75,33 @@ const handleFailure2 = (rejectReason) => {
 checkInventory(order).then(handleSuccess).catch(handleFailure);
 
 /// /// /// /// ///
+
+const { checkInventory, processPayment, shipOrder } = require("./library.js");
+
+const order3 = {
+  items: [
+    ["sunglasses", 1],
+    ["bags", 2],
+  ],
+  giftcardBalance: 79.82,
+};
+
+checkInventory(order)
+  .then((resolvedValueArray) => {
+    // Write the correct return statement here:
+    return processPayment(resolvedValueArray);
+  })
+  .then((resolvedValueArray) => {
+    // Write the correct return statement here:
+    return shipOrder(resolvedValueArray);
+  })
+  .then((successMessage) => {
+    console.log(successMessage);
+  })
+  .catch((errorMessage) => {
+    console.log(errorMessage);
+  });
+
+  /// /// /// /// ///
+
+  
