4aa5b82aaf3881dbce0f427c738c98deea760158
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-15 14:59:08 +0100

    .

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-21.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-21.js
@@ -102,6 +102,29 @@ checkInventory(order)
     console.log(errorMessage);
   });
 
-  /// /// /// /// ///
+/// /// /// /// ///
 
-  
+const { checkInventory, processPayment, shipOrder } = require("./library.js");
+
+const order4 = {
+  items: [
+    ["sunglasses", 1],
+    ["bags", 2],
+  ],
+  giftcardBalance: 79.82,
+};
+
+// Refactor the code below:
+
+checkInventory(order)
+  .then((resolvedValueArray) => {
+    return processPayment(resolvedValueArray);
+  })
+  .then((resolvedValueArray) => {
+    return shipOrder(resolvedValueArray);
+  })
+  .then((successMessage) => {
+    console.log(successMessage);
+  });
+
+  /// /// /// /// ///
