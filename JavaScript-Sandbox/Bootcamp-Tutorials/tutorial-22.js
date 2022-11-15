3160163fa5a8aec04f05efff84c1bbd25c8a40e8
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-15 15:05:45 +0100

    The await Operator

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-22.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-22.js
@@ -89,3 +89,25 @@ async function announceDinner() {
 
 announceDinner();
 
+/// /// /// /// ///
+
+const brainstormDinner = () => {
+  return new Promise((resolve, reject) => {
+    console.log(`I have to decide what's for dinner...`);
+    setTimeout(() => {
+      console.log("Should I make salad...?");
+      setTimeout(() => {
+        console.log("Should I make ramen...?");
+        setTimeout(() => {
+          console.log("Should I make eggs...?");
+          setTimeout(() => {
+            console.log("Should I make chicken...?");
+            resolve("beans");
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  });
+};
+
+module.exports = brainstormDinner;
