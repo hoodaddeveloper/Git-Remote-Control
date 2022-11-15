cb7c61b2ab503a383c615e082342a501fb2bc223
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-15 15:06:43 +0100

    Writing async Functions

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-22.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-22.js
@@ -111,3 +111,32 @@ const brainstormDinner = () => {
 };
 
 module.exports = brainstormDinner;
+
+/// /// /// /// ///
+
+const shopForBeans = require("./library.js");
+
+async function getBeans() {
+  console.log(`1. Heading to the store to buy beans...`);
+  let value = await shopForBeans();
+  console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
+}
+
+getBeans();
+
+const shopForBeans = () => {
+  return new Promise((resolve, reject) => {
+    const beanTypes = ["kidney", "fava", "pinto", "black", "garbanzo"];
+    setTimeout(() => {
+      let randomIndex = Math.floor(Math.random() * beanTypes.length);
+      let beanType = beanTypes[randomIndex];
+      console.log(`2. I bought ${beanType} beans because they were on sale.`);
+      resolve(beanType);
+    }, 1000);
+  });
+};
+
+module.exports = shopForBeans;
+
+/// /// /// /// ///
+
