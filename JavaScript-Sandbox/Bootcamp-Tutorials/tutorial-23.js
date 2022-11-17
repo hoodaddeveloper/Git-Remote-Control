b8b709cec06507f47d12df7a9e37851216b7e2ec
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-18 00:51:36 +0100

    Making an async POST Request

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-23.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-23.js
@@ -318,3 +318,47 @@ const renderJsonResponse6 = (res) => {
   // Manipulates responseField to show the returned JSON.
   responseField.innerHTML = `<pre>${rawJson}</pre>`;
 };
+
+// information to reach API
+const apiKey33 = '<Your API Key>';
+const url33 = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField33 = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField33 = document.querySelector('#responseField');
+
+// Asynchronous functions
+// Code goes here
+const shortenUrl33 = async () => {
+	const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(url, {
+			method: 'POST',
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+				'apikey': apiKey
+      }
+    });
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl33 = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
