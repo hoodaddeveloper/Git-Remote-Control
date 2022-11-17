590539503b22478991c43a69f85e925c0d44fde7
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-18 00:43:00 +0100

    Handling a POST Request

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-23.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-23.js
@@ -145,4 +145,53 @@ const displayShortUrl3 = (event) => {
   shortenUrl();
 };
 
+shortenButton3.addEventListener("click", displayShortUrl);
+
+// Information to reach API
+const apiKey = "<Your API Key>";
+const url = "https://api.rebrandly.com/v1/links";
+
+// Some page elements
+const inputField = document.querySelector("#input");
+const shortenButton = document.querySelector("#shorten");
+const responseField = document.querySelector("#responseField");
+
+// Asynchronous functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({ destination: urlToShorten });
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      apikey: apiKey,
+    },
+    body: data,
+  })
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Request failed!");
+      },
+      (networkError) => {
+        console.log(networkError.message);
+      }
+    )
+    .then((jsonResponse) => {
+      renderResponse(jsonResponse);
+    });
+};
+
+// Clear page and call Asynchronous functions
+const displayShortUrl2 = (event) => {
+  event.preventDefault();
+  while (responseField.firstChild) {
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+};
+
 shortenButton.addEventListener("click", displayShortUrl);
