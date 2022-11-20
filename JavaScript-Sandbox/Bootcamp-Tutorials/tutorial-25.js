731153aac75937f9de1e8f97df9a3f56b13d1f79
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-20 14:32:17 +0100

    .

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-25.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-25.js
@@ -10,3 +10,22 @@ console.log("Because of the error, this will not printed!");
 console.log(Error("User missing name"));
 console.log("Will logging the error stop our program from running?");
 
+throw Error('Username or password does not match');
+
+/* try {
+  throw Error('Practicing throwing');
+} catch (e) {
+  console.log(e);
+} */
+
+function capAllElements(arr){
+  try{
+  	arr.forEach((el, index, array) => {
+      array[index] = el.toUpperCase();
+    });
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+/* capAllElements('Incorrect argument'); */
