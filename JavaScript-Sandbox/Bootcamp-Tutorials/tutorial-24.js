b8a9af8c9272b99f1380330b1e7c152bb7831304
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-20 14:17:24 +0100

    Finding Documentation

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-24.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-24.js
@@ -80,3 +80,12 @@ console.log(
   "getLaterFirstLetter('astro', 'afar') returns: " +
     getLaterFirstLetter("astro", "afar")
 );
+
+// Link to String.repeat() documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+function doubleString(string) {
+  return string.repeat(2);
+}
+
+// Should return 'echoecho'
+console.log("doubleString('echo') returns: " + doubleString("echo"));
+
