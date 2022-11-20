e849f62a961418b25b510f0913e1e1878679cbab
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-11-20 14:19:58 +0100

    stringLength

--- JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-24.js
+++ JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-24.js
@@ -106,3 +106,11 @@ console.log(
     containsCake("Pie is certainly the coolest dessert.")
 );
 
+// Should return true
+console.log("isStringPerfectLength('Dog', 2, 4) returns: " + isStringPerfectLength('Dog', 2, 4));
+
+// Should return false
+console.log("isStringPerfectLength('Mouse', 2, 4) returns: " + isStringPerfectLength('Mouse', 2, 4));
+
+// Should return false
+console.log("isStringPerfectLength('Cat', 4, 9) returns: " + isStringPerfectLength('Cat', 4, 9));
